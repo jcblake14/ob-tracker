720bf5a1cbe44880eb546dec3527593f810d54bf
Jessica Blake <jcblake14@gmail.com>
2017-09-24 21:00:43 -0400

    correct env variables

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  // for deployment
+  // for deployment purposes
   process.env.HEROKU_POSTGRESQL_CHARCOAL_URL || 'postgres://localhost:5432/ob', {
     logging: false
   }
