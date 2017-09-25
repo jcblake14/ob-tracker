d188706f6a964e8df8f97e4ff9589661ff0cbdc0
Jessica Blake <jcblake14@gmail.com>
2017-09-24 20:56:17 -0400

    add database

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  // for deploy
-  process.env.DATABASE_URL || 'postgres://localhost:5432/ob', {
+  // for deployment
+  process.env.HEROKU_POSTGRESQL_CHARCOAL_URL || 'postgres://localhost:5432/ob', {
     logging: false
   }
 )
