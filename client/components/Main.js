e20daabd11d7a01950d53d6532c02d92d2527333
Jessica Blake <jcblake14@gmail.com>
2017-09-28 00:33:23 -0400

    dont pass props to main

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,11 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
-import {logout} from '../store'
-import {Navbar} from '../components'
-import {Container} from './styled'
-import theme from '../theme'
 
 export default function Main(props){
   const {children} = props
